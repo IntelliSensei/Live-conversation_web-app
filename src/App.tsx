416ff05e18bb84dee604852723a9482980d1e288
KP <ace_643@hotmail.com>
2022-09-22 16:07:19 +0200

    worked on UseState/Effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TextField } from "./components/input";
 import { DropDown, IOption, ColorPalette } from "./components/input";
 import Panel from "./components/Panel";
 
-const options: IOption[] = [
-  { key: "key1", value: "my val 1" },
-  { key: "key2", value: "my val 2" },
-  { key: "key3", value: "my val 3" },
-  { key: "key4", value: "my val 4" },
-  { key: "key5", value: "my val 5" },
-  { key: "key6", value: "my val 6" },
-];
+interface IValues {
+  one?: string;
+  two?: string;
+}
+
 
 export default function App() {
+
+  // const [text1, setText1] = useState("");
+  // const [text2, setText2] = useState("");
+  const [values, setValues] = useState<IValues>({});
+  useEffect(() => {
+    const strValues = sessionStorage.getItem("values");
+    if(strValues){
+      const data = JSON.parse(strValues);
+      setValues(data);
+      return;
+    }
+    setValues({one:"missing", two:"missing"})
+   }, [])
+
   return (
     <div>
-      <TextField label="test" />
       <TextField
+        label="Textfield 1"
+        onChange={(nv) => {
+          setValues({ ...values, one: nv })
+        }}
+      />
+      <TextField
+        label="Textfield 2"
         placeholder="12313"
         onChange={(nv) => {
-          console.log("nv", nv);
+          setValues({ ...values, two: nv })
         }}
       />
-      <DropDown
-        label="color"
-        options={options}
-        onChange={(nv) => {
-          console.log("dd", nv);
-        }}
-      />
+      <button onClick={() => {
+        sessionStorage.setItem("values", JSON.stringify(values));
+
+      }}>Send</button>
+      <div>
+        <p>text 1: {values.one} </p>
+        <p>text 2: {values.two} </p>
+      </div>
       <Panel />
     </div>
   );
--- src/components/input/TextField.tsx
+++ src/components/input/TextField.tsx
@@ -33,7 +33,6 @@ export const TextField: FC<ITextFieldProps> = ({
         style={{height: "3vh", width: "100%", margin: "0", padding: "5px", borderRadius:"3px", border: "1px solid #80b3ff"}}
         onChange={(event) => {
           const newVal = event.target.value;
-          console.log({ newVal });
           if (onChange) onChange(newVal);
         }}  
         placeholder={placeholder}
