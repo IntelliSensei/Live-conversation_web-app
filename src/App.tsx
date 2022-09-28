96b66e789e6287c9c273415938f06954d717291c
Bardia Jedi <bardiajedi@gmail.com>
2022-09-28 15:09:55 +0200

    added conversations

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
-import React, { useEffect } from "react";
-import useWebSocket from "react-use-websocket";
+import React, { useEffect, useState } from "react";
 import { TextBubble } from "./components/output/TextBubble";
 import { Panel } from "./components/Panel";
-import { useLocalStorage } from "./hooks/useLocalStorage";
-
-interface IValues {
-  one?: string;
-  two?: string;
-}
+import { useConversation } from "./hooks/useConversation";
 
 export default function App() {
-  const socketUrl = "ws://localhost:8999";
+  const [c, setC] = useState(0);
 
-
-  const { sendJsonMessage } = useWebSocket(socketUrl)
-
-
+  const { sendConversation, conversations } = useConversation(
+    "ws://localhost:8999"
+  );
 
   return (
     <div>
-      {/* <TextBubble
-        alias={JSON.stringify(values.one)} // need real values. for example alias = socketResponse.alias
-        color={JSON.stringify("red")}
-        message={JSON.stringify(values.two)}
-      /> */}
-      <Panel
-        // onChange={(nv) => console.log("onChange", nv)}
-        onMessageChange={(nv) => {
-          const sendObject = {...nv, Atype: "CONVERSATION"}
-          console.log(sendObject)
-          sendJsonMessage(sendObject)
-        }}
-      />
+      <button onClick={() => setC(c + 1)}>add {c}</button>
+      <div>
+        {Object.values(conversations).map(c => <TextBubble {...c}/>)}
+      </div>
+      <Panel onMessageChange={sendConversation} />
     </div>
   );
 }
