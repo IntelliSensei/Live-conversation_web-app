101f07cd2b9c829815a47865822996310ada63fc
Bardia Jedi <bardiajedi@gmail.com>
2022-10-07 10:28:19 +0200

    added get text bubble info

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React, { useRef, useState } from "react";
-import { TextBubble } from "./components/output/TextBubble";
+import React, { useEffect, useRef, useState } from "react";
+import { TextField } from "./components/input";
+import { ITextBubbleInfo, TextBubble } from "./components/output/TextBubble";
 import { Panel } from "./components/Panel";
 import { useConversation } from "./hooks/useConversation";
 
-
 export default function App() {
-  // const [c, setC] = useState(0);
-
+  const [textBubbleInfo, setTextBubbleInfo] = useState<
+    Record<string, () => ITextBubbleInfo>
+  >({});
   const { sendConversation, conversations } = useConversation(
     "ws://localhost:8999"
   );
+
+  useEffect(() => {
+    for (const key in textBubbleInfo) {
+      const cb = textBubbleInfo[key];
+      console.log(key, cb());
+    }
+  }, [conversations]);
 
   return (
     <div>
       {/* <button onClick={() => setC(c + 1)}>add {c}</button> */}
       <div className="test">
-        {Object.values(conversations).map(c => <TextBubble {...c} />)}
+        {Object.values(conversations).map((c) => (
+          <TextBubble
+            {...c}
+            getInfo={(cb) =>
+              setTextBubbleInfo({ ...textBubbleInfo, [c.id]: cb })
+            }
+          />
+        ))}
       </div>
       <Panel onMessageChange={sendConversation} />
     </div>
-  )
+  );
 }
-
--- src/components/output/TextBubble.tsx
+++ src/components/output/TextBubble.tsx
@@ -1,50 +1,75 @@
-import React, { FC, useEffect, useMemo, useState } from "react";
-import { IWSMessagePKG } from "../Panel"
-import "../css/TextBubble.css"
+import React, { FC, useCallback, useEffect, useMemo, useState } from "react";
+import "../css/TextBubble.css";
 
+export interface ITextBubbleInfo {
+  width: number;
+  height: number;
+  top: number;
+  left: number;
+  timeToLive: number;
+}
 
 interface ITextBubbleProps {
-    alias: string;
-    color: string;
-    message: string;
-    // onMessageChange?: (newValue: IWSMessagePKG) => void;
+  alias: string;
+  color: string;
+  message: string;
+  getInfo?: (myInfo:() => ITextBubbleInfo) => void;
 }
 
 function getRandomNumber(min: any, max: any) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
 function randomizeBubbles() {
-    var winWidth = window.innerWidth - 500;
-    var winHeight = window.innerHeight - 200;
-    var randomTop = getRandomNumber(0, winHeight);
-    var randomLeft = getRandomNumber(0, winWidth);
+  var winWidth = window.innerWidth - 500;
+  var winHeight = window.innerHeight - 200;
+  var randomTop = getRandomNumber(0, winHeight);
+  var randomLeft = getRandomNumber(0, winWidth);
 
-    return { top: randomTop + "px", left: randomLeft + "px" }
+  return { top: randomTop + "px", left: randomLeft + "px" };
 }
 
-export const TextBubble: FC<ITextBubbleProps> = ({ alias, color, message }: ITextBubbleProps) => {
+export const TextBubble: FC<ITextBubbleProps> = ({
+  alias,
+  color,
+  message,
+  getInfo,
+}: ITextBubbleProps) => {
 
-    const positions = useMemo(() => randomizeBubbles(), [])
-    const [timeToLive, setTimeToLive] = useState(10)
+  const positions = useMemo(() => randomizeBubbles(), []);
+  const [timeToLive, setTimeToLive] = useState(10);
 
-    useEffect(() => {
-        let ttl = 10;
-        console.log("set interval", alias);
-        const id = setInterval(() => {
-            console.log("test", alias, ttl)
-            ttl = ttl - 1;
-            setTimeToLive(ttl);
-        }, 1000);
-        return () => clearInterval(id)
-    }, [message])
+  const myInfo = useCallback(() => {
+    return {
+      timeToLive,
+      top: Number(positions.top.replace("px","")),
+      left: Number(positions.left.replace("px", "")),
+      height: 300,
+      width: 150,
+    } as ITextBubbleInfo;
+  }, [timeToLive, positions]);
 
-    if (timeToLive < 0)
-        return <div></div>;
-    return <section className="textBubble" style={{ backgroundColor: color, ...positions }}>
-        <h3 className="bubble-alias">{alias}</h3>
-        <p className="bubble-msg">
-            {message}
-        </p>
+  useEffect(() => getInfo && getInfo(myInfo), [myInfo])
+
+  useEffect(() => {
+      let ttl = 60;
+      console.log("set interval", alias);
+      const id = setInterval(() => {
+          console.log("test", alias, ttl)
+          ttl = ttl - 1;
+          setTimeToLive(ttl);
+      }, 1000);
+      return () => clearInterval(id)
+  }, [message])
+
+  if (timeToLive < 0) return <div></div>;
+  return (
+    <section
+      className="textBubble"
+      style={{ backgroundColor: color, ...positions }}
+    >
+      <h3 className="bubble-alias">{alias}</h3>
+      <p className="bubble-msg">{message}</p>
     </section>
-}
+  );
+};
