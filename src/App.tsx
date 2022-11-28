04e42b99f98164b7d3c1c207361dd18ad8200655
Jesperere <jesper_sylte@msn.com>
2022-11-28 09:14:24 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,17 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
-import React, { useEffect, useState } from "react";
-import { ITextBubbleInfo, TextBubble } from "./components/output/TextBubble";
-import { Panel } from "./components/Panel";
-import { useConversation } from "./hooks/useConversation";
 import "./components/css/App.css"
-import { LoginField } from "./components/login/Login";
+import { ConversationApp } from "./components/conversationApp";
 
 export default function App() {
-  const [textBubbleInfo, setTextBubbleInfo] = useState<
-    Record<string, () => ITextBubbleInfo>
-  >({});
-  const { sendConversation, conversations, removeConversations } = useConversation(
-    "ws://localhost:8999"
-  );
-
 
   const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
+    uri: 'http://localhost:4000/', // should come from dotenv
     cache: new InMemoryCache(),
-  });
-
-  useEffect(() => {
-    if (Object.values(conversations).length < 1) return;
-
-    for (const key in textBubbleInfo) {
-      const bubbleInfo = textBubbleInfo[key]();
-      if (bubbleInfo.timeToLive < -2) {
-        removeConversations(key)
-        delete textBubbleInfo[key];
-        setTextBubbleInfo(textBubbleInfo)
-      }
-    }
-
-  }, [conversations]);
+});
 
   return (
     <ApolloProvider client={client}>
-      <div>
-        <div className="bubble-container">
-          {Object.values(conversations).map((c) => (
-            <TextBubble
-              {...c}
-              key={c.id}
-              getInfo={(cb) =>
-                setTextBubbleInfo({ ...textBubbleInfo, [c.id]: cb })
-              }
-            />
-          ))}
-        </div>
-        <Panel onMessageChange={sendConversation} />
-        {/* <ListAllUsers /> */}
-        <div className="top-bar">
-          <LoginField />
-          {/* <SignUpField
-            alias="JEsper"
-            password="asdasd"
-            email="asdasd@mail.com"
-          /> */}
-        </div>
-      </div>
+      <ConversationApp />
     </ApolloProvider>
   );
 }
